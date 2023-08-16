43242a09785d427c794c6698a3584b8294c4839b
EOlaw <Emmanuel.ao@outlook.com>
2023-08-16 00:55:23 -0500

    update

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+
+//const userRoutes = require('./routes/userRoutes')
+
+
+app.listen(4000, () => {
+    console.log('listening on 4000')
+})
